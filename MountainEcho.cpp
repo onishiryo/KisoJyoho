e61cecbf2ea6679688711c2d900121d9843d5339
onishi ryo <onishi.ryo@gsic.titech.ac.jp>
2022-05-22 15:17:55 +0900

    VSonly commented

--- MountainEcho.cpp
+++ MountainEcho.cpp
@@ -5,9 +5,9 @@ int main(void) {
     char out_str[20];
 
     scanf("%5s", in_str); //キー入力のうち5文字まで受入れて in_str に代入
-//    scanf_s("%5s", in_str, 6); //キー入力のうち5文字まで受入れて in_str に代入
+//    scanf_s("%5s", in_str, 6); //VSonly
     sprintf(out_str, "%s %s %.3s %s", in_str, in_str, in_str, "...");
-//    sprintf_s(out_str, "%s %s %.3s %s", in_str, in_str, in_str, "...");
+//    sprintf_s(out_str, "%s %s %.3s %s", in_str, in_str, in_str, "..."); //VSonly
     printf("%s", out_str);
 
     return 0;
--- MountainEchoIO.cpp
+++ MountainEchoIO.cpp
@@ -9,17 +9,22 @@ int main(void) {
 
     //input
     infp = fopen("infile.txt", "r");
+//    fopen_s(&infp, "infile.txt", "r"); //VSonly
     fscanf(infp, "%5s", in_str); //infpから文字をスキャンして、うち5文字までを in_str に代入
+//    fscanf_s(infp, "%5s", in_str, 6); //VSonly
     printf("input string=%s\n", in_str);
     fclose(infp);
 
     //MountainEcho
     sprintf(out_str, "%s %s %.3s %s", in_str, in_str, in_str, "...");
+//    sprintf_s(out_str, "%s %s %.3s %s", in_str, in_str, in_str, "..."); //VSonly
     printf("output string=%s\n", out_str);
 
     //output
     outfp = fopen("outfile.txt", "w");
+//    fopen_s(&outfp, "outfile.txt", "w"); //VSonly
     fprintf(outfp, "%s", out_str);
+//    fprintf_s(outfp, "%s", out_str); //VSonly
     fclose(outfp);
 
     return 0;
