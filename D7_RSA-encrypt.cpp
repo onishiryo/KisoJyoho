fd2ded2937e2f446b248131db5547db56a00054b
onishi ryo <onishi.ryo@gsic.titech.ac.jp>
2022-05-30 00:50:56 +0900

    long int =>long lon

--- D7_RSA-encrypt.cpp
+++ D7_RSA-encrypt.cpp
@@ -2,38 +2,40 @@
 #include <stdio.h>
 #include <math.h>
 
-long int PowerMod(long int a, long int k, long int n);
+long long PowerMod(long long a, long long k, long long n);
 
 int main() {
-    long int n=5183; //公開鍵 n=p*q
-    long int e=11; //公開鍵 e
-    long int d=2291; //秘密鍵 d
-    long int inum_input=754; //input number
+    long long n=5183; //公開鍵 n=p*q
+    long long e=11; //公開鍵 e
+    long long d=2291; //秘密鍵 d
+    long long inum_input=754; //input number
+
+    long long inum_encrypted, inum_decoded;
 
     int imode=2; //1=encode, 2=decode
     switch(imode){
         case 1: //encoding, e&n required
-          printf("Encoding the number: %ld\n", inum_input);
-          long int inum_encrypted = PowerMod(inum_input, e, n);
-          printf("encrypted number=%lu\n",inum_encrypted);
+          printf("Encoding the number: %lld\n", inum_input);
+          inum_encrypted = PowerMod(inum_input, e, n);
+          printf("encrypted number=%lld\n",inum_encrypted);
           break;
         case 2: //decoding, d&n required
-          printf("Decoding the number: %ld\n", inum_input);
-          long int inum_decoded = PowerMod(inum_input, d, n);
-          printf("(decoded) plain number=%ld\n",inum_decoded);          
+          printf("Decoding the number: %lld\n", inum_input);
+          inum_decoded = PowerMod(inum_input, d, n);
+          printf("(decoded) plain number=%lld\n",inum_decoded);          
           break;
     }
 
     return 0;
 }
 
-long int PowerMod(long int a, long int k, long int n) {
+long long PowerMod(long long a, long long k, long long n) {
 /*
 単純にa^k (mod n)を計算したいところだけど、
 a^kが非常に大きい数字になり得るので、少し工夫する（繰り返し自乗法）。
 */
-    long int i;
-    long int value = 1;
+    long long i;
+    long long value = 1;
     a = a%n;
     for(i = 0; i < k; i++) {
         value = value*a;
