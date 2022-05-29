33400d1f505d3df39d8843c9c61307f17239ae4d
onishi ryo <onishi.ryo@gsic.titech.ac.jp>
2022-05-30 00:14:00 +0900

    long int =>long long

--- D7_pn-search.cpp
+++ D7_pn-search.cpp
@@ -4,11 +4,11 @@
 #include <math.h> //to use math functions, e.g., sqrt, sqrtf, pow, powf
 #include <time.h> //for time
 
-long int check_prime (long int);
+long long check_prime (long long);
 
 int main(void) {
-    long int ix, ipf=0;
-    long int ix_start, ix_end;
+    long long ix, ipf=0;
+    long long ix_start, ix_end;
     time_t tstart, tend;    
 
     FILE *outfp;
@@ -17,15 +17,15 @@ int main(void) {
     tstart = time(NULL); //for computing time measurement
     ix_start=1; // 奇数であるべき。18桁以下。
     //        1234567890123456789
-    ix_end  =100000000;
+    ix_end = 1000000; // 100000000;
 
     if((ix_start)%2==0){ix_start=ix_start+1;} //for safety
 
     for(ix=ix_start; ix<=ix_end; ix=ix+2){        
         ipf = check_prime(ix);
         if(ipf==0){
-            printf("%ld is a prime number.\n", ix);
-            fprintf(outfp, "%ld\n", ix);}}
+            printf("%lld is a prime number.\n", ix);
+            fprintf(outfp, "%lld\n", ix);}}
 
     fclose(outfp);
     tend = time(NULL); //for computing time measurement
@@ -34,11 +34,11 @@ int main(void) {
     return 0; 
 }
 
-long int check_prime(long int inum){
-    long int i, isqrt_num, ipfactor;
+long long check_prime(long long inum){
+    long long i, isqrt_num, ipfactor;
 
     ipfactor=0;
-    isqrt_num=(long int)sqrt((double)inum);
+    isqrt_num=(long long)sqrt((double)inum);
 
     for(i=3; i<=isqrt_num; i=i+2){
         if((inum%i)==0){
